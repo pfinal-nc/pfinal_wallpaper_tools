8eabe661a4ab314ca9892e5fc831d7a8fdca1242
PFinal_南丞 <1370288056@qq.com>
2023-10-19 11:32:38 +0800

    2023-10-18 init code

---
+++ README.md
@@ -0,0 +1,5 @@
+# pfinal_wallpaper_tools
+
+基于 go wails 和 layvue 构建的 桌面换壁纸小工具
+
+
---
+++ frontend/src/main.js
@@ -0,0 +1,6 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import Layui from '@layui/layui-vue'
+import './style.css';
+
+createApp(App).use(Layui).mount('#app')
